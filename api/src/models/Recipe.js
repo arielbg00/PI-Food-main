752d000d57d0fc4c5b3aeaf80f64b400790c16fb
arielbg00 <ariel-bravo96@hotmail.com>
2022-10-19 21:44:24 -0300

    Changes 19/10/2022 21:40

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -4,9 +4,33 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    summary: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    healthScore: {
+      type: DataTypes.INTEGER
+    },
+    steps: {
+      type: DataTypes.STRING
+    },
+    image: {
+      type: DataTypes.STRING,
+      defaultValue: ""
+    },
+    created: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true
+    }
   });
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const axios = require("axios");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -7,6 +8,11 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+router.get("/recipes", async (req, res) => {
+   const getApi = await axios.get("https://api.spoonacular.com/recipes/complexSearch?addRecipeInformation=true&apiKey=bee23743640145c3bcf6664e2c031ec4");
+   res.json(getApi.data);
+});
 
 
 module.exports = router;
